f4921406eab0f69ddf15bb9dffec7aa1c8e39488
Javier <javier.flores@tecsup.edu.pe>
2021-04-13 23:44:04 -0500

    Agenda - Punto 7: Filtro

--- agenda/src/components/App.js
+++ agenda/src/components/App.js
@@ -4,8 +4,10 @@ const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas', number: '345-34444-23'}
   ]) 
+  const [ personsCopia, setPersonsCopia ] = useState([...persons]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter] = useState('')
   const manejarSubmit = (e) => {
     e.preventDefault();
     let repetido = ([...persons].filter((person)=>person.name===newName)).length
@@ -13,6 +15,7 @@ const App = () => {
       alert(`El nombre ${newName} ya fué agregado`)
     }else{
       setPersons([...persons, {name: newName, number: newNumber}])
+      setPersonsCopia([...personsCopia, {name: newName, number: newNumber}])
     }
     setNewName("")
     setNewNumber("")
@@ -23,10 +26,16 @@ const App = () => {
   const manejarCambioNumero = (e) => {
     setNewNumber(e.target.value)
   }
+  const manejarCambioFiltro = async(e) => {
+    await setNewFilter(e.target.value)
+    setPersons([...personsCopia].filter((person)=>person.name.toLowerCase().includes(e.target.value.toLowerCase())))
+  }
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with: <input onChange={manejarCambioFiltro} value={newFilter}/>
       <form onSubmit={manejarSubmit}>
+        <h2>add a new</h2>
         <div>
           name: <input onChange={manejarCambioNombre} value={newName}/><br/>
           number: <input onChange={manejarCambioNumero} value={newNumber}/>
@@ -37,7 +46,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person)=><p key={person.name}>{person.name}: {person.number}</p>)}
+        {(persons.length > 0) ? persons.map((person)=><p key={person.name}>{person.name}: {person.number}</p>) : <p>No hay contactos con este nombre</p>}
       </div>
     </div>
   )
