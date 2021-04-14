77f34a7712561d277a61c54f91e8491efa0886c4
Javier <javier.flores@tecsup.edu.pe>
2021-04-13 23:13:30 -0500

    Agenda - Punto 6: Numeros de telefono

--- agenda/src/components/App.js
+++ agenda/src/components/App.js
@@ -2,28 +2,34 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '345-34444-23'}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
   const manejarSubmit = (e) => {
     e.preventDefault();
     let repetido = ([...persons].filter((person)=>person.name===newName)).length
     if (repetido > 0){
       alert(`El nombre ${newName} ya fué agregado`)
     }else{
-      setPersons([...persons, {name: newName}])
+      setPersons([...persons, {name: newName, number: newNumber}])
     }
     setNewName("")
+    setNewNumber("")
   }
-  const manejarCambio = (e) => {
+  const manejarCambioNombre = (e) => {
     setNewName(e.target.value)
+  }
+  const manejarCambioNumero = (e) => {
+    setNewNumber(e.target.value)
   }
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={manejarSubmit}>
         <div>
-          name: <input onChange={manejarCambio} value={newName}/>
+          name: <input onChange={manejarCambioNombre} value={newName}/><br/>
+          number: <input onChange={manejarCambioNumero} value={newNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -31,7 +37,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person)=><p key={person.name}>{person.name}</p>)}
+        {persons.map((person)=><p key={person.name}>{person.name}: {person.number}</p>)}
       </div>
     </div>
   )
