d63affb99b0ad0f85488f52c65eb23ea4651db7c
Javier <javier.flores@tecsup.edu.pe>
2021-04-13 22:30:18 -0500

    Cursos - Parte 2: Suma de los ejercicios

--- cursos/src/components/Content.js
+++ cursos/src/components/Content.js
@@ -4,6 +4,7 @@ const Content = (props) => {
     return (
     <>
         {props.partes.map((parte)=><Part key={parte.id} parte={parte}/>)}
+        <p><b>Total de ejercicios: </b>{props.partes.reduce((acum,{exercises: valor})=> acum + valor, 0)}</p>
     </>
     )
 }
