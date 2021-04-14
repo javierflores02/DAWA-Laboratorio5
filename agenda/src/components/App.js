919925b94d46827b900b1e1c6331d7edb7621026
Javier <javier.flores@tecsup.edu.pe>
2021-04-13 22:52:40 -0500

    Agenda - Punto 4: Creación de proyecto e inicialización

---
+++ agenda/src/components/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const manejarSubmit = (e) => {
+    e.preventDefault();
+    setPersons([...persons, {name: newName}])
+    setNewName("")
+  }
+  const manejarCambio = (e) => {
+    setNewName(e.target.value)
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={manejarSubmit}>
+        <div>
+          name: <input onChange={manejarCambio} value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person)=><p key={person.name}>{person.name}</p>)}
+      </div>
+    </div>
+  )
+}
+
+export default App
